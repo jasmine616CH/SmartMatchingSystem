6c4dab041daea996f9a7737377e43bcbec7ad5ed
jasmine <2552882728@qq.com>
2026-07-22 21:13:30 +0800

    first commit

---
+++ src/main/java/org/example/smartmatchsystem/SmartMatchSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.smartmatchsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMatchSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartMatchSystemApplication.class, args);
+    }
+
+}
